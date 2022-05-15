7cb81c47e0c065fdff4dfe4e726f910f9efae3de
kat-kalyyan <32221979+kat-kalyyan@users.noreply.github.com>
2022-05-15 17:05:27 +0530

    initial commit

---
+++ src/test/java/com/example/paytmdemo/PaytmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paytmdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
